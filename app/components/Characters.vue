862d481ec5dab3a1704fc23920d8c5a7e6cee4de
SHatice <sarah.gungor@hetic.net>
2019-05-16 19:07:54 +0200

    begin characters page behaviour

---
+++ app/components/Characters.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{ gameData.charactersPage.content }}</h1>
+    <ul>
+      <li>
+        <div>
+          {{ gameData.charactersPage.characters[0].class }}
+        </div>
+      </li>
+      <li>
+        <div>
+          {{ gameData.charactersPage.characters[1].class }}
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import game from "../data.json";
+
+export default {
+  data() {
+    return {
+      gameData: game
+    };
+  }
+};
+</script>
--- app/router.js
+++ app/router.js
@@ -2,9 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Home from './components/Home.vue';
-import Page from './components/Page.vue';
 import Game from './components/Game.vue';
-import Character from './components/Character.vue'
+import Characters from './components/Characters.vue';
 
 Vue.use(Router);
 
@@ -18,9 +17,9 @@ const router = new Router({
       component: Home,
     },
     {
-      path: '/character',
-      name: 'character',
-      component: Character,
+      path: '/characters',
+      name: 'characters',
+      component: Characters,
     },
     {
       path: '/game/:id',
