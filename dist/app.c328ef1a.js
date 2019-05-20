c18e3b586f5c71acd6f789c77dfc3c2a7d6d83f5
hlnmargary <margary.helene@hetic.net>
2019-05-20 16:09:46 +0200

    remove cache

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -15965,7 +15965,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57254" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58528" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
