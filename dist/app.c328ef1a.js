7b3e43d0f0a7ff7bd934d1e34230204165da3e24
hlnmargary <margary.helene@hetic.net>
2019-05-23 15:47:23 +0200

    push on master

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -42248,7 +42248,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60738" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50257" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
