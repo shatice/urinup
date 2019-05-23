7dcebba39f9f2edf247b2229545df9f92f02f9fb
hlnmargary <margary.helene@hetic.net>
2019-05-23 15:45:19 +0200

    oups

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -17095,7 +17095,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57169" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49914" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
