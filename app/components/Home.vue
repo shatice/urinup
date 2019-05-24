25c160370485fc399ad89f6af5a2c99952d4a817
SHatice <sarah.gungor@hetic.net>
2019-05-24 21:37:02 +0200

    add more content to json

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -4,6 +4,9 @@
       <svg class="loader" aria-hidden="true"><use xlink:href="#loader"></use></svg>
     </div>
     <div class="homeContent">
+      <div class="logoContainer">    
+        <svg class="logo" aria-hidden="true"><use xlink:href="#logo"></use></svg>
+      </div>
       <h1 class="playContent">{{ message }}</h1>
       <router-link class="buttonHome" to="/characters">Jouer</router-link>
     </div>
